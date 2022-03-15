013a7b7b16cbdd123e102bc4a1523cf030d7b050
Divyan8h <99311459+Divyan8h@users.noreply.github.com>
2022-03-15 11:13:43 +0530

    Create Selection sort.java

---
+++ Selection sort.java
@@ -0,0 +1,32 @@
+package session1;
+
+class selection{
+  public static void main(String args[]){
+    //selection sort
+	int arr[] = {7,8,3,1,2};
+	
+	//outer loop
+	for(int i=0;i<arr.length-1;i++)
+	{
+		//Inner loop
+		int smallest = i;
+		//j=i+1 - because all the elemnets before i is sorted
+		for(int j=i+1;j<arr.length;j++)
+		{
+			if(arr[smallest]>arr[j])
+			{
+				smallest = j;
+			}
+		}
+		int temp = arr[smallest];
+		arr[smallest] = arr[i];
+		arr[i] = temp;
+	}
+	
+	for(int k=0;k<arr.length;k++)
+	{
+		System.out.print(arr[k] + " ");
+	}
+	System.out.println();
+}
+}
